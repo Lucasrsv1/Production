a4feca7dddc1cd8e5e07391d828f2cb6005391b9
Lucas Rassilan Vilanova <lucasrsv1@gmail.com>
2017-05-19 18:18:41 -0300

    Bug fix.

--- public/js/Maquina.js
+++ public/js/Maquina.js
@@ -2,7 +2,9 @@
  * Created by tcs on 19/05/17.
  */
 
-// TODO: adicionar status atual e incluir a duração no tempo.
+/*  TODO: adicionar status atual e incluir a duração no tempo.
+ *	Learn: https://www.codeschool.com/courses/try-docker
+ */
 
 var Maquina = function (velocidade_producao) {
 	var evento_inicio = null;
@@ -61,32 +63,38 @@ var Maquina = function (velocidade_producao) {
 }
 
 function FormatarTempo (ms, includeMS) {
-	var meses = ms / 2592000000;
+	var anos = Math.floor(ms / 946080000000);
+	ms -= anos * 946080000000;
+
+	var meses = Math.floor(ms / 2592000000);
 	ms -= meses * 2592000000;
 	
-	var dias = ms / 86400000;
+	var dias = Math.floor(ms / 86400000);
 	ms -= dias * 86400000;
 	
-	var horas = ms / 3600000;
+	var horas = Math.floor(ms / 3600000);
 	ms -= horas * 3600000;
 	
-	var minutos = ms / 60000;
+	var minutos = Math.floor(ms / 60000);
 	ms -= minutos * 60000;
 	
-	var segundos = ms / 1000;
+	var segundos = Math.floor(ms / 1000);
 	ms -= segundos * 1000;
 	
 	var result = "";
-	if (meses > 0)
+	if (anos > 0)
+		result += anos.toString() + (anos > 1 ? " anos " : " ano ");
+
+	if (meses > 0 || anos > 0)
 		result += meses.toString() + (meses > 1 ? " meses " : " mês ");
 	
-	if (dias > 0 || meses > 0)
+	if (dias > 0 || meses > 0 || anos > 0)
 		result += dias.toString() + (dias > 1 ? " dias " : " dia ");
 
-	if (horas > 0 || dias > 0 || meses > 0)
+	if (horas > 0 || dias > 0 || meses > 0 || anos > 0)
 		result += horas.toString() + " hr ";
 
-	if (minutos > 0 || horas > 0 || dias > 0 || meses > 0)
+	if (minutos > 0 || horas > 0 || dias > 0 || meses > 0 || anos > 0)
 		result += minutos.toString() + "min ";
 
 	result += segundos.toString() + "sec";
